3e2dbe9f922a7e71d53777a1ea1d1509a7acd711
jrcmadushanka <43390258+jrcmadushanka@users.noreply.github.com>
2022-03-14 14:24:16 +0530

    Game logic
    Load views before running

--- MyApplicationdeck/app/src/main/java/com/example/myapplicationdeck/MainActivity.java
+++ MyApplicationdeck/app/src/main/java/com/example/myapplicationdeck/MainActivity.java
@@ -9,12 +9,44 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
 
-    ImageView one , tow , three ,four ,five ,six,seven,eight,nine,ten,result;
-    Button roll;
-
+   // ImageView one , tow , three ,four ,five ,six,seven,eight,nine,ten,result;
+   
+    Button roll = binding.ivBackButton;
+    
+    ImageView result = binding.ivBackButton;
+    ImageView[] images = {roll, roll, roll, roll, roll, roll, roll, roll, roll, };
+    ImageView userSelection = null;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        for(ImageView image : images){
+            image.setOnClickListener(view -> {
+                userSelection = (ImageView) view;
+            });
+        }
+
+        roll.setOnClickListener(view -> {
+            try {
+                ImageView selection = images[new Random().nextInt(images.length - 1)];
+
+                if(selection != null) {
+                    result.setImageDrawable(selection.getDrawable());
+
+                    if(userSelection != null){
+                        if (userSelection.getId() == selection.getId()){
+                            Toast.makeText(this, "You Won!", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(this, "You Lost!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+
+            } catch (Exception exception){
+                exception.printStackTrace();
+            }
+        });
     }
 }
